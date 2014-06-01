3b2f904a0e0a350255f3cdf7e55b4622d97e709c
Miguel Serrano <Mimi@Mimi.local>
2014-06-01 22:17:29 +0200

    Initial Commit

---
+++ Drawing a House/MSViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSViewController.h
+//  Drawing a House
+//
+//  Created by Miguel Serrano on 01/06/14.
+//  Copyright (c) 2014 Miguel Serrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSViewController : UIViewController
+
+@end
---
+++ Drawing a HouseTests/Drawing_a_HouseTests.m
@@ -0,0 +1,34 @@
+//
+//  Drawing_a_HouseTests.m
+//  Drawing a HouseTests
+//
+//  Created by Miguel Serrano on 01/06/14.
+//  Copyright (c) 2014 Miguel Serrano. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Drawing_a_HouseTests : XCTestCase
+
+@end
+
+@implementation Drawing_a_HouseTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
